4f27d1c71be57b5eddac320bff4ab3b7c23c95f6
Nditah <nditah@gmail.com>
2021-12-05 10:11:25 +0100

    first commit

---
+++ decorator/pizza/ToppingDecorator.java
@@ -0,0 +1,7 @@
+package designpatterns.decorator.pizza;
+
+public abstract class ToppingDecorator extends Pizza {
+	Pizza pizza;
+
+	public abstract String getDescription();
+}
---
+++ factory/challenge/Zone.java
@@ -0,0 +1,14 @@
+package designpatterns.factory.challenge;
+
+public class Zone {
+	String displayName;
+	int offset;
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+}
